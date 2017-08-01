b833ebcb7088a59a7bbbdeae1bd7526d8dd98669
Michael Wickey <wickeym@gmail.com>
2017-08-01 10:24:23 -0400

    FIX: Add correct runtimeCaching urls (external files like bootstrap)

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,3 +1,4 @@
+// Cache external https://stackoverflow.com/questions/40587733/caching-external-resources-with-sw-precache
 module.exports = {
   navigateFallback: '/index.html',
   stripPrefix: 'dist',
@@ -9,7 +10,17 @@ module.exports = {
     'dist/**.css'
   ],
   runtimeCaching: [{
-    urlPattern: '*',
+    urlPattern: /^https:\/\/api\.wickeyappstore\.com\//,
     handler: 'networkFirst'
-  }]
+  }, {
+    urlPattern: /^https:\/\/maxcdn\.bootstrapcdn\.com\//,
+    handler: 'cacheFirst'
+  }, {
+    urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/fastclick\/1\.0\.6\/fastclick\.min\.js/,
+    handler: 'cacheFirst'
+  }, {
+    urlPattern: /^https:\/\/unpkg\.com\/wickeyappstore@0\.5\.2\/styles\.css/,
+    handler: 'cacheFirst'
+  }],
+  navigateFallback: '/index.html',
 };
