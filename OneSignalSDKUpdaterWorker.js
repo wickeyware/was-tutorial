a506f53aad463771e3209baa0e2327955d103784
Michael Wickey <wickeym@gmail.com>
2017-08-28 17:03:58 -0400

    FIX: Do not re-show push permission modal after push enabled

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
-// <do not remove line>[029126f0-7443-473b-a1ec-42429b1a3609]
+// <do not remove line>[cfed5fbd-7546-42dc-9e51-fed414b94679]
 // IMPORT PROJECT SERVICE WORKER
 importScripts('service-worker.js');
 // IMPORT OneSignal FOR PUSH NOTIFICATIONS
