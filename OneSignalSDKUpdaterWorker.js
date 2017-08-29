a9fcaa382219b8cbc1834e8cd0004a7106010a53
Michael Wickey <wickeym@gmail.com>
2017-08-29 09:44:19 -0400

    CHANGES VERSION 0.7.3
    FIX: Add `was-tutorial` to path for OneSignal Updater js file

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
-// <do not remove line>[cfed5fbd-7546-42dc-9e51-fed414b94679]
+// <do not remove line>[b2a65d8d-91fe-4986-934e-f026fe5007b2]
 // IMPORT PROJECT SERVICE WORKER
 importScripts('service-worker.js');
 // IMPORT OneSignal FOR PUSH NOTIFICATIONS
