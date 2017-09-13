a16675d6f68eb79959e7003c278057f12da4f855
Michael Wickey <wickeym@gmail.com>
2017-09-13 16:14:06 -0400

    CHANGES VERSION 1.0.1
    FIX: Use relative url for airhorn sound

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,4 +1,4 @@
-// <do not remove line>[3eeb6198-0bf7-4332-81d0-02f6ba13f0bb]
+// <do not remove line>[1117c366-6dcd-45fb-be3e-49a5628c00ed]
 // IMPORT PROJECT SERVICE WORKER
 importScripts('service-worker.js');
 // IMPORT OneSignal FOR PUSH NOTIFICATIONS
