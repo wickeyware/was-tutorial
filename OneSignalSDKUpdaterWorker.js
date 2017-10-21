9bddd5ca75252fadc41ec0502682f7e4100bbe21
Michael Wickey <wickeym@gmail.com>
2017-10-21 14:16:15 -0400

    CHANGES VERSION 1.1.6
    FIX: Fix bug that did not allow selection of textarea

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
-// <do not remove line>[35cb7f4b-3d17-4c12-bff5-416afbf2ac65]
+// <do not remove line>[0fa199ab-f123-4c3b-9fac-41c20adabc31]
 // IMPORT PROJECT SERVICE WORKER
 importScripts('service-worker.js');
 // IMPORT OneSignal FOR PUSH NOTIFICATIONS
